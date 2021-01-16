5c782b9e364258776bb65c008b6d054bdac250d1
Choi Seok Ung <scsc0511@naver.com>
2021-01-16 18:00:34 +0000

    study step 10 for test of auto-grad

---
+++ chapter1/step10_test.py
@@ -0,0 +1,112 @@
+import numpy as np
+import unittest
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Variable:
+    def __init__(self, data):
+        if (data is not None) and not isinstance(data ,np.ndarray):
+            raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+        self.data = data 
+        self.grad = None 
+        self.creator = None 
+
+    def set_creator(self, creator):
+        self.creator = creator 
+
+    def backward(self):
+        if self.grad == None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while len(funcs) :
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+class Function : 
+    def __call__(self, inp):
+       x = inp.data
+       y = self.forward(x)
+
+       outp = Variable(as_array(y))
+       outp.set_creator(self)
+
+       self.input = inp
+       self.output = outp
+
+       return outp
+
+    def forward(self, x):
+        raise NotImplementedError
+ 
+    def backward(self, gy):
+        raise NotImplementedError 
+
+class Square(Function):
+    def forward(self, x):
+        return x**2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2*x*gy
+
+        return gx
+         
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+ 
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x)*gy
+        return gx   
+
+def numerical_diff(f, x, eps=1e-04):
+    x0 = Variable(x.data-eps)
+    x1 = Variable(x.data+eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data)/(x1.data - x0.data)
+
+class SquareTest(unittest.TestCase):
+    #def test_forward(self):
+        #x = Variable(np.array(2.0))
+        #y = square(x)
+        #expected = np.array(3.0)
+        #self.assertEqual(y.data, expected)
+
+    #def test_backward(self):
+    #    x = Variable(np.array(3.0))
+    #    y = square(x)
+    #    y.backward()
+    #    expected = np.array(6.0)
+    #    self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        # Random하게 입력 값 생성 
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        # np.allclose(a, b, rtol=1e-05, atol=1e-08)
+        # (a-b) <= (atol + rtol*|b|)를 보장  
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+unittest.main()
